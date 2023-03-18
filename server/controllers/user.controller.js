9b890f8a5f03e014fceceb89591bd1fc186bc682
Andhika Prasetya <andhikapr081@gmail.com>
2023-03-18 21:51:19 +0700

    server working with createUser controller save user data to mongodb, if user isn't exist yet

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,7 +1,26 @@
-import User from '../mongodb/models/user';
+import User from '../mongodb/models/user.js';
 
 const getAllUsers = async (req, res) => {};
-const createUser = async (req, res) => {};
+const createUser = async (req, res) => {
+  try {
+    const { name, email, avatar } = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    // don't create user, if user already exists
+    if (userExists) return res.status(200).json(userExists);
+
+    const newUser = await User.create({
+      name,
+      email,
+      avatar,
+    });
+
+    res.status(200).json(newUser);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 const getUserInfoByID = async (req, res) => {};
 
 export { getAllUsers, createUser, getUserInfoByID };
