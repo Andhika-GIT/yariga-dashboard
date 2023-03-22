3f6c9309369f49cbd44010e59b9b267af30db833
Andhika Prasetya <andhikapr081@gmail.com>
2023-03-22 18:44:28 +0700

    client render agents

--- client/src/pages/agent.tsx
+++ client/src/pages/agent.tsx
@@ -1,5 +1,41 @@
-const agent = () => {
-  return <div>agent</div>;
+import { useList } from '@pankod/refine-core';
+import { Box, Typography } from '@pankod/refine-mui';
+
+import { AgentCard } from 'components';
+
+const Agent = () => {
+  const { data, isLoading, isError } = useList({
+    resource: 'users',
+  });
+
+  const allAgents = data?.data ?? [];
+
+  if (isLoading) return <div>loading...</div>;
+  if (isError) return <div>error...</div>;
+
+  console.log(allAgents);
+
+  return (
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color="#11142d">
+        Agent list
+      </Typography>
+
+      <Box
+        mt="2opx"
+        sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          gap: '20px',
+          backgroundColor: '#fcfcfc',
+        }}
+      >
+        {allAgents.map((agent) => (
+          <AgentCard key={agent._id} id={agent._id} name={agent.name} email={agent.email} avatar={agent.avatar} noOfProperties={agent.allProperties.length} />
+        ))}
+      </Box>
+    </Box>
+  );
 };
 
-export default agent;
+export default Agent;
