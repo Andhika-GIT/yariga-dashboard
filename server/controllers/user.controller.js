8d9730d587eb8dbb99c10cfdccef6b8c38005df2
Andhika Prasetya <andhikapr081@gmail.com>
2023-03-22 19:27:29 +0700

    server getting user data by id param

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,6 +1,14 @@
 import User from '../mongodb/models/user.js';
 
-const getAllUsers = async (req, res) => {};
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find({}).limit(req.query._end);
+
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 const createUser = async (req, res) => {
   try {
     const { name, email, avatar } = req.body;
@@ -21,6 +29,20 @@ const createUser = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
-const getUserInfoByID = async (req, res) => {};
+const getUserInfoByID = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const user = await User.findOne({ _id: id }).populate('allProperties');
+
+    if (!user) {
+      throw new Error('user not found');
+    }
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
 export { getAllUsers, createUser, getUserInfoByID };
