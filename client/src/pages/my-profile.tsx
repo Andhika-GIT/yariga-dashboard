f9d814c94f5c86d5f79bc13fbe34d62dbce5fb32
Andhika Prasetya <andhikapr081@gmail.com>
2023-03-22 19:27:12 +0700

    client display my-profile and agents list

--- client/src/pages/my-profile.tsx
+++ client/src/pages/my-profile.tsx
@@ -1,5 +1,20 @@
-const myProfile = () => {
-  return <div>myProfile</div>;
+import { useGetIdentity, useOne } from '@pankod/refine-core';
+
+import { Profile } from 'components';
+
+const MyProfile = () => {
+  const { data: user } = useGetIdentity();
+  const { data, isLoading, isError } = useOne({
+    resource: 'users',
+    id: user?.userId,
+  });
+
+  const myProfile = data?.data ?? [];
+
+  if (isLoading) return <div>loading...</div>;
+  if (isError) return <div>error...</div>;
+
+  return <Profile type="My" name={myProfile.name} email={myProfile.email} avatar={myProfile.avatar} properties={myProfile.allProperties} />;
 };
 
-export default myProfile;
+export default MyProfile;
